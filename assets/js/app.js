117f3cf72500fd97298e43e71e3ea20963bca4af
erobertsdev <elias@eroberts.dev>
2020-07-06 14:41:25 -0600

    DOM renders correct letters

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 const words = [ 'Oliver', 'Clementine', 'Bear', 'Beats', 'Bottle', 'Peach', 'Rabbit', 'Bat' ],
 	gameGraphicDOM = document.querySelector('.game-graphic'),
-	gameWordDOM = document.querySelector('.game-word');
+	gameWordDOM = document.querySelector('.game-word'),
+	guessText = document.getElementById('guess-text'),
+	guessButton = document.getElementById('guess-button');
 
 const gameState = {
 	gameOver: true,
@@ -23,14 +25,40 @@ const gameController = {
 		console.log(`Word set to ${gameState.word}`);
 	},
 	renderWordDOM: (word) => {
-		for (let letter of word) {
+		for (let i = 0; i < word.length; i++) {
 			const letterSpace = document.createElement('div');
-			letterSpace.innerHTML = `<div class="game-letter">_</div>`;
-			gameState.wordLetters.push(letter);
+			letterSpace.innerHTML = `<div class="game-letter" id="letter-${i}">_</div>`;
+			gameState.wordLetters.push(word[i].toLowerCase());
 			gameWordDOM.appendChild(letterSpace);
+		}
+	},
+	renderCorrectGuess: (letter, i) => {
+		console.log(letter, i);
+	},
+	checkGuess: (letter) => {
+		const wordArr = gameState.wordLetters;
+		// Check if letter is in wordLetters array
+		if (!wordArr.includes(letter)) {
+			console.log(`Word does not include a ${letter}! :(`);
+		} else {
+			for (let i = 0; i < wordArr.length; i++) {
+				if (wordArr[i] === letter) {
+					// Render correctly guessed letter in DOM
+					console.log(`Found a ${letter} at position ${i}.`);
+					document.getElementById(
+						`letter-${i}`
+					).innerHTML = `<div class="game-letter" id="letter-${i}">${letter}</div>`;
+				}
+			}
 		}
 	}
 };
+
+guessButton.addEventListener('click', (e) => {
+	e.preventDefault();
+	console.log(guessText.value.toLowerCase());
+	gameController.checkGuess(guessText.value.toLowerCase());
+});
 
 gameController.startGame();
 console.log(gameState);
