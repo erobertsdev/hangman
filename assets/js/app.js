e258bbfcb1c6fb657a1294d8d37d772efbbeef89
erobertsdev <elias@eroberts.dev>
2020-07-06 12:36:12 -0600

    renders spaces for each letter of word

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,7 @@ const gameState = {
 	gameOver: true,
 	word: '',
 	guessedLetters: [],
+	wordLetters: [],
 	playerGuessCount: 0
 };
 
@@ -14,12 +15,20 @@ const gameController = {
 		let idx = Math.floor(Math.random() * arr.length);
 		return arr[idx];
 	},
-
 	startGame() {
 		gameState.word = this.setGameWord(words);
+		this.renderWordDOM(gameState.word);
 		gameState.playerGuessCount = 0;
 		gameState.gameOver = false;
 		console.log(`Word set to ${gameState.word}`);
+	},
+	renderWordDOM: (word) => {
+		for (let letter of word) {
+			const letterSpace = document.createElement('div');
+			letterSpace.innerHTML = `<div class="game-letter">_</div>`;
+			gameState.wordLetters.push(letter);
+			gameWordDOM.appendChild(letterSpace);
+		}
 	}
 };
 
